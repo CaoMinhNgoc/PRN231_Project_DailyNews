935527808b02a63a5eba4ad98b65ce9cac1c4c0a
Isaac Cao <107320915+CaoMinhNgoc@users.noreply.github.com>
2023-11-11 22:31:23 +0700

    add Identity

--- DailyNews.Web/Program.cs
+++ DailyNews.Web/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using DailyNews.BusinessObject.DataContext;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DailyNewsContextConnection") ?? throw new InvalidOperationException("Connection string 'DailyNewsContextConnection' not found.");
+
+builder.Services.AddDbContext<DailyNewsContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DailyNewsContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
