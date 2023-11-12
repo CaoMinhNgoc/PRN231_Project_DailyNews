31977d1d2a3ecef53396181b6d8c85d333655ead
Isaac Cao <107320915+CaoMinhNgoc@users.noreply.github.com>
2023-11-13 01:19:22 +0700

    add article detail

---
+++ DailyNews.Web/wwwroot/js/article-detail.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function (){
+    ShowArticleDetail();
+    ShowArticleComments();
+
+    function ShowArticleDetail() {
+        $.ajax({
+            url: "http://localhost:5166/api/Articles/GetArticleById/16",
+            type: "get",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (result, status, xhr) {
+                const formattedDate = new Date(result.createdDate).toLocaleString();
+
+                $("#created-date").text("Created at: " + formattedDate);
+                $("#title").text(result.title);
+                $("#short-description").text(result.shortDescription);
+                $("#content").text(result.content);
+            },
+        })
+    }
+
+    function ShowArticleComments() {
+        $.ajax({
+            url: "http://localhost:5166/api/Comments/GetCommentsByArticleId/16",
+            type: "get",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (result, status, xhr) {
+                const commentsContainer = $('#comments');
+                $.each(result, function (index, value) {
+                    const commentElement = $("<div>");
+                    const timeElement = $("<i>").text("Commented at: " + new Date(value.commentTime).toLocaleString());
+                    const contentElement = $("<p>").text(value.commentContent);
+                    commentElement.append(timeElement);
+                    commentElement.append(contentElement);
+                    commentsContainer.append(commentElement);
+                });
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr);
+            }
+        })
+    }
+})
